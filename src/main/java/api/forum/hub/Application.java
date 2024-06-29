5e2b4b002359a059bdf2698e8c8bd5edb74a63c7
RebecaCamilo <rebecacamilo3@gmail.com>
2024-06-29 11:34:41 -0300

    build: start project

---
+++ src/main/java/api/forum/hub/Application.java
@@ -0,0 +1,13 @@
+package api.forum.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

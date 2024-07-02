11ec7d16d59ec7c80c759c09f00abbfb5cc8e72d
RebecaCamilo <rebecacamilo3@gmail.com>
2024-07-02 19:29:52 -0300

    refac: remove datasource username and password from properties

--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -2,7 +2,7 @@ create table topicos (
     id bigserial not null primary key,
     titulo varchar(100) not null,
     mensagem varchar(500) not null,
-    dataCriacao timestamp not null,
+    data_criacao timestamp not null,
     status smallint not null default 1,
     autor varchar(100) not null,
     curso varchar(100) not null

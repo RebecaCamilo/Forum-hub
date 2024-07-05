50dce1d63ba771b576d686bb218f6cc0dad15574
RebecaCamilo <rebecacamilo3@gmail.com>
2024-07-05 01:09:28 -0300

    feat: implements UserDetailsService in AutenticacaoService class

---
+++ src/main/java/api/forum/hub/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package api.forum.hub.repository;
+
+import api.forum.hub.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}

54c81b83275837115842baa4f5c62fd44d54b886
RebecaCamilo <rebecacamilo3@gmail.com>
2024-07-05 01:08:58 -0300

    feat: add Usuario domain object

---
+++ src/main/java/api/forum/hub/domain/Usuario.java
@@ -0,0 +1,21 @@
+package api.forum.hub.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
